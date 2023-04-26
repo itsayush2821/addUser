9d4052fca33d839509906503678ecde89dac9169
itsayush2821 <annuj0028@gmail.com>
2023-04-27 01:22:11 +0530

    localStorage_delete

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ let form = document.getElementById('addUser')
 
 let userList = document.getElementById('userList')
 
+var itemList = document.getElementById('userList');
+
+
 form.addEventListener('submit', addItem);
+
+itemList.addEventListener('click', removeItem)
 
 function addItem(e){
     e.preventDefault()
@@ -16,6 +21,17 @@ function addItem(e){
     var li = document.createElement('li');
 
     li.appendChild(document.createTextNode(name+"-"+email+"-"+phone));
+
+    var deleteBtn = document.createElement('button');
+
+    deleteBtn.className = 'delete';
+
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+
+    li.appendChild(deleteBtn);
+
+
+
 
     userList.appendChild(li)
     
@@ -36,3 +52,25 @@ function addItem(e){
     }
 
 }
+
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+        console.log(e.target.parentElement.textContent)
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+
+        let userList = JSON.parse(localStorage.getItem('user'))
+        for(let i=0;i<userList.length;i++){
+            let fullName = userList[i].name+"-"+userList[i].email+"-"+userList[i].phone+"Delete"
+            console.log(fullName)
+            if(fullName===e.target.parentElement.textContent){
+                userList.pop(userList[i])
+                break;
+            }
+        }
+        localStorage.setItem('user',JSON.stringify(userList))
+      }
+    }
+  }
