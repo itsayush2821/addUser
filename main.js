b974b0fa44e21f872deb0c2f1065c090389a41d7
itsayush2821 <annuj0028@gmail.com>
2023-04-28 11:42:29 +0530

    editButton

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ let form = document.getElementById('addUser')
 
 let userList = document.getElementById('userList')
 
-var itemList = document.getElementById('userList');
+let itemList = document.getElementById('userList');
 
+let edit = document.getElementsByClassName('edit')
 
 form.addEventListener('submit', addItem);
 
 itemList.addEventListener('click', removeItem)
+
+// if(edit!=null){
+//     edit.addEventListener('click', removeItem)
+// }
+
 
 function addItem(e){
     e.preventDefault()
@@ -29,6 +35,14 @@ function addItem(e){
     deleteBtn.appendChild(document.createTextNode('Delete'));
 
     li.appendChild(deleteBtn);
+
+    var editBtn = document.createElement('button');
+
+    editBtn.className = 'edit';
+
+    editBtn.appendChild(document.createTextNode('Edit'));
+
+    li.appendChild(editBtn);
 
 
 
@@ -55,6 +69,37 @@ function addItem(e){
 
 
 function removeItem(e){
+    let name = document.getElementById('name')
+    let email = document.getElementById('email')
+    let phone = document.getElementById('phone')
+    console.log(e.target.classList)
+
+    if(e.target.classList.contains('edit')){
+       
+            console.log(e.target.parentElement.textContent)
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+    
+            let userList = JSON.parse(localStorage.getItem('user'))
+            let newUserList = []
+            for(let i=0;i<userList.length;i++){
+                let fullName = userList[i].name+"-"+userList[i].email+"-"+userList[i].phone+"Delete"+"Edit"
+                console.log(fullName)
+                console.log(e.target.parentElement.textContent)
+                if(fullName===e.target.parentElement.textContent){
+                    // userList.pop(userList[i])
+                    name.value=userList[i].name
+                    email.value=userList[i].email
+                    phone.value=userList[i].phone
+                    
+                }else{
+                    newUserList.push(userList[i])
+                }
+            }
+            localStorage.setItem('user',JSON.stringify(newUserList))
+          }   
+    
+
     if(e.target.classList.contains('delete')){
       if(confirm('Are You Sure?')){
         console.log(e.target.parentElement.textContent)
@@ -62,15 +107,22 @@ function removeItem(e){
         itemList.removeChild(li);
 
         let userList = JSON.parse(localStorage.getItem('user'))
+        let newUserList = []
         for(let i=0;i<userList.length;i++){
-            let fullName = userList[i].name+"-"+userList[i].email+"-"+userList[i].phone+"Delete"
+            let fullName = userList[i].name+"-"+userList[i].email+"-"+userList[i].phone+"Delete"+"Edit"
             console.log(fullName)
+            console.log(e.target.parentElement.textContent)
             if(fullName===e.target.parentElement.textContent){
-                userList.pop(userList[i])
-                break;
+                // userList.pop(userList[i])
+                name.value=userList[i].name
+                email.value=userList[i].email
+                phone.value=userList[i].phone
+                
+            }else{
+                newUserList.push(userList[i])
             }
         }
-        localStorage.setItem('user',JSON.stringify(userList))
+        localStorage.setItem('user',JSON.stringify(newUserList))
       }
     }
-  }
+}
